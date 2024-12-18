1e400eacc19db7b04216c2eca1bff117c6dbce1e
Erisukmawan <erisukmawan03@gmail.com>
2024-12-18 18:40:15 +0700

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
                     class="navbar navbar-expand-lg blur border-radius-lg top-0 z-index-3 shadow position-absolute mt-4 py-2 start-0 end-0 mx-4">
                     <div class="container-fluid">
                         <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="{{route('welcome')}}">
-                            <img src="{{ asset('/assets') }}/guest/images/logo.png" class="img-fluid" style="height: 2em;" alt="main_logo">
+                            <img src="{{ asset('/assets') }}/guest/images/Logo.png" class="img-fluid" style="height: 2em;" alt="main_logo">
                         </a>
                         <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
